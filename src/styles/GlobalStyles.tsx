2df3d58154a08d2776931742f096c02248b00863
zoel-a <zulmyazhary32@gmail.com>
2022-12-16 22:55:11 +0800

    Setup the assets

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -2,14 +2,47 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   :root {
-    /* Breakpoints */
+    //! Breakpoints
     --mobile: ${(props) => props.theme.media.mobile};
     --tablet: ${(props) => props.theme.media.tablet};
     --laptop: ${(props) => props.theme.media.laptop};
     --desktop: ${(props) => props.theme.media.desktop};
 
-    /* Colors */
-    --background: ${(props) => props.theme.colors.background};
+    //! Colors
+    /* Bg */
+    --bgPaper: ${(props) => props.theme.colors.background.paper};
+    --bgDefault: ${(props) => props.theme.colors.background.default};
+
+    /* Text */
+    --textPrimary: ${(props) => props.theme.colors.text.primary};
+    --textSecondary: ${(props) => props.theme.colors.text.secondary};
+    --textDisabled: ${(props) => props.theme.colors.text.disabled};
+
+    /* Primary */
+    --primaryMain: ${(props) => props.theme.colors.primary.main};
+    --primaryDark: ${(props) => props.theme.colors.primary.dark};
+    --primaryLight: ${(props) => props.theme.colors.primary.light};
+
+    /* Secondary */
+    --secondaryMain: ${(props) => props.theme.colors.secondary.main};
+
+    /* Action */
+    --actionActive: ${(props) => props.theme.colors.action.active};
+    --actionDisabled: ${(props) => props.theme.colors.action.disabled};
+    --actionDisabledBackground: ${(props) => props.theme.colors.action.disabledBackground};
+
+    /* Others */
+    --contrast: ${(props) => props.theme.colors.others.contrast};
+    --divider: ${(props) => props.theme.colors.others.divider};
+
+    /* Components */
+    --outline: ${(props) => props.theme.colors.components.outline};
+
+    /* Neutral */
+    --neutral900: ${(props) => props.theme.colors.neutral[900]};
+    --neutral500: ${(props) => props.theme.colors.neutral[500]};
+    --neutral400: ${(props) => props.theme.colors.neutral[400]};
+    --neutral300: ${(props) => props.theme.colors.neutral[300]};
   }
 
   * {
@@ -19,7 +52,20 @@ export default createGlobalStyle`
   }
 
   body {
-    background-color: var(--background);
+    background-color: var(--bgDefault);
     font-family: Inter, sans-serif;
+  }
+  
+  h1, h2, h3, h4, h5, h6 {
+    line-height: 137.5%;
+    color: var(--textPrimary);
+  }
+
+  h1, h2, h3, h4 {
+    font-weight: 700;
+  }
+
+  h5, h6 {
+    font-weight: 600;
   }
 `;
--- styled.d.ts
+++ styled.d.ts
@@ -7,11 +7,47 @@ type Media = {
   desktop: string;
 }
 
+type Colors = {
+  background: {
+    paper: string;
+    default: string;
+  };
+  text: {
+    primary: string;
+    secondary: string;
+    disabled: string;
+  };
+  primary: {
+    main: string;
+    dark: string;
+    light: string;
+  };
+  secondary: {
+    main: string;
+  };
+  action: {
+    active: string;
+    disabled: string;
+    disabledBackground: string;
+  };
+  others: {
+    contrast: string;
+    divider: string;
+  };
+  components: {
+    outline: string;
+  };
+  neutral: {
+    900: string;
+    500: string;
+    300: string;
+    400: string;
+  }
+}
+
 declare module "styled-components" {
   export interface DefaultTheme {
     media: Media;
-    colors: {
-      background: string;
-    }
+    colors: Colors;
   }
 }
